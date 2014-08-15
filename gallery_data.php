e346be85ec4f202d5be373b517bb0b8394608724
sb5 <steveb533@hotmail.com>
2014-08-15 17:12:50 -0400

    Update gallery_data.php

--- gallery_data.php
+++ gallery_data.php
@@ -1,38 +1,32 @@
 <?php
 
-  
-
 
 function loadImages(){ 
 
 $number = 4;
-$img_dir = '/images/';//change here
+$img_dir = '/images/2014_conf';//change here
 $dir = $_SERVER['DOCUMENT_ROOT'].$img_dir;
 $scan = scandir($dir);
 $imgCount=0;
 $json = '{"directory":"'.$img_dir.'","total_count":"';//start it
-if(!$_SESSION['img_list']){
+
 
 
 foreach($scan as $candidate){
 
 if(preg_match('/.+\.(jpeg|jpg|gif|png|JPG)$/',$candidate)){
 
-$imgCount++;
-$_SESSION['img_list'][] = $candidate;
 
+   $imgArray[] = array('id'=>$imgCount, 'imgUrl'=>$candidate);
+$imgCount++;
 }
 }
-}
+
 $json .= $imgCount;
  $json .= '","image_array":';
 
 
- $current_ones = $_SESSION['img_list'];
 
-foreach($current_ones as $k=>$v){
-             $imgArray[] = array('id'=>$k, 'imgUrl'=>$v);
-            }
           $a = json_encode($imgArray);
  
         
