057fefff9172770e75b4a166977249bf7d4c0573
sb5 <steveb533@hotmail.com>
2014-08-15 14:08:16 -0400

    Update gallery_data.php

--- gallery_data.php
+++ gallery_data.php
@@ -1,23 +1,24 @@
 <?php
 
 
-
+echo $_SERVER['PHP_SELF'];
   
 
 
 function loadImages(){ 
 
 $number = 4;
-$dir = $_SERVER['DOCUMENT_ROOT'].'/shop/images';
+$img_dir = '/images/';//change here
+$dir = $_SERVER['DOCUMENT_ROOT'].$img_dir;
 $scan = scandir($dir);
 $imgCount=0;
-$json = '{"total_count":"';//start it
+$json = '{"directory":"'.$img_dir.'","total_count":"';//start it
 if(!$_SESSION['img_list']){
 
 
 foreach($scan as $candidate){
 
-if(preg_match('/.+\.(jpeg|jpg|gif|png)$/',$candidate)){
+if(preg_match('/.+\.(jpeg|jpg|gif|png|JPG)$/',$candidate)){
 
 $imgCount++;
 $_SESSION['img_list'][] = $candidate;
